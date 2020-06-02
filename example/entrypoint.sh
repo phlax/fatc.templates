589166766dd24d206183ef6eeddfad1eeff3c8e7
Ryan Northey <ryan@synca.io>
2020-06-03 00:19:54 +0100

    Add hashtree dependency

--- example/entrypoint.sh
+++ example/entrypoint.sh
@@ -4,9 +4,11 @@
 ls /tmp/dist
 
 apt update
+apt install -y -qq --no-install-recommends gpg gpg-agent ca-certificates software-properties-common wget
+wget -qO - https://phlax.github.io/debian/gpg | apt-key add -
+apt-add-repository "deb [arch=amd64] https://phlax.github.io/debian buster main"
+apt update
 apt install -y -qq --no-install-recommends /tmp/dist/tpls_*deb
-
 echo "FINISHED INSTALLING TPLS!"
-
 
 exec ${@}
